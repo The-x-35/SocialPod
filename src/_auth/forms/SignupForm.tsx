180359b199442fb18a7d9682c8ac766c782b3799
The-x-35 <arpit.k3note@gmail.com>
2024-03-02 18:47:02 +0530

    Sign up ready and working properly

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button"
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
@@ -10,15 +10,19 @@ import {
 import { Input } from "@/components/ui/input"
 import { SignupValidation } from '@/lib/validation'
 import Loader from '@/components/shared/loader';
-import { createUserAccount } from '@/lib/appwrite/api';
 import { useToast } from "@/components/ui/use-toast"
+import { useCreateUserAccount, useSignInAccount } from "@/lib/react-query/queriesAndMutations";
+import { useUserContext } from "@/context/AuthContext";
 
 
 
 const SignupForm = () => {
 
-  const isLoading = false;
   const { toast } = useToast()
+  const { checkAuthUser, isLoading: isUserloading } = useUserContext();
+  const navigate = useNavigate();
+  const { mutateAsync: createUserAccount, isPending: isCreatingAccount } = useCreateUserAccount();
+  const { mutateAsync: signInAccount, isPending: isSigningIn } = useSignInAccount();
   // 1. Define your form.
   const form = useForm<z.infer<typeof SignupValidation>>({
     resolver: zodResolver(SignupValidation),
@@ -37,7 +41,20 @@ const SignupForm = () => {
     if(!newUser){
       return toast({title: "Sign up failed. Please try again."})
     }
-    // const session =await signInAccount()
+    const session =await signInAccount({
+      email: values.email,
+      password: values.password,
+    })
+    if(!session){
+      return toast({ title: 'Sign in failed. Please try again.'})
+    }
+    const isLoggedIn = await checkAuthUser();
+    if(isLoggedIn){
+      form.reset();
+      navigate('/')
+    } else{
+      return toast({title: 'Sign up failed. Please try again.'})
+    }
 
   }
   return (
@@ -104,7 +121,7 @@ const SignupForm = () => {
         )}
       />
       <Button type="submit" className='shad-button_primary'>
-        {isLoading ? (
+        {isCreatingAccount ? (
           <div className='flex-center gap-2'>
             <Loader />Loading...
           </div>
--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,8 @@
 import { INewUser } from "@/types";
 import { account, appwriteConfig, avatars, databases } from "./config";
-import { ID } from "appwrite";
+import { ID, Query } from "appwrite";
+
+
 
 export async function createUserAccount(user: INewUser) {
     try {
@@ -29,7 +31,7 @@ export async function createUserAccount(user: INewUser) {
     }
 }
 
-export async function saveUsertoDB(user:{
+export async function saveUserToDB(user:{
     accountId: string;
     email: string;
     name: string;
@@ -46,5 +48,32 @@ export async function saveUsertoDB(user:{
         return newUser;
     }catch(error){
         console.log(error);
+    }
+}
+
+export async function signInAccount(user: { email: string; password: string;}) {
+    try{
+        const session = await account.createEmailSession(user.email,user.password);
+        return session;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function getCurrentUser() {
+    try {
+        const currentAccount = await account.get();
+        if(!currentAccount) throw Error;
+        const currentUser = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.userCollectionId,
+            [Query.equal("accountId", currentAccount.$id)]
+        );
+        if(!currentUser) throw Error;
+
+        return currentUser.documents[0];
+    } catch (error) {
+        console.log(error);
+        return null;
     }
 }
