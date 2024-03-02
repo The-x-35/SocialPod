ca4973b344900153ac430b8982ebbdd3d4c6c1af
The-x-35 <arpit.k3note@gmail.com>
2024-03-03 02:01:04 +0530

    Topbar setup done

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -77,3 +77,12 @@ export async function getCurrentUser() {
         return null;
     }
 }
+
+export async function signOutAccount(){
+    try {
+        const session= await account.deleteSession("current");
+        return session;
+    } catch (error) {
+        console.log(error);
+    }
+}
--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -5,7 +5,7 @@ import {
     useQueryClient,
     useInfiniteQuery,
 } from '@tanstack/react-query'
-import { createUserAccount, signInAccount } from '../appwrite/api'
+import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
 
 export const useCreateUserAccount = () => {
     return useMutation({
@@ -19,5 +19,11 @@ export const useSignInAccount = () => {
             email: string;
             password: string;
         }) => signInAccount(user),
+    });
+}
+
+export const useSignOutAccount = () => {
+    return useMutation({
+        mutationFn: signOutAccount
     });
 }
